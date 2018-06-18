65b6b228e76b989a9fcddf38676388588937a964
Viktoriia Badisova <vbadisova@gmail.com>
2018-06-18 13:30:07 +0300

    Set Up the Core Data Stack

---
+++ Notes/Managers/CoreDataManager.swift
@@ -0,0 +1,54 @@
+//
+//  CoreDataManager.swift
+//  Notes
+//
+//  Created by Виктория Бадисова on 15.06.2018.
+//  Copyright © 2018 Виктория Бадисова. All rights reserved.
+//
+
+import CoreData
+
+final class CoreDataManager {
+    
+    private let modelName: String
+    
+    init(modelName: String) {
+        self.modelName = modelName
+    }
+    
+    private(set) lazy var managedObjectContext: NSManagedObjectContext = {
+        let managedObjectContext = NSManagedObjectContext(concurrencyType: .mainQueueConcurrencyType)
+        managedObjectContext.persistentStoreCoordinator = self.persistentStoreCoordinator
+        return managedObjectContext
+    }()
+    
+    private lazy var managedObjectModel: NSManagedObjectModel = {
+        guard let modelURL = Bundle.main.url(forResource: self.modelName, withExtension: "momd") else {
+            fatalError("Unable to find data model")
+        }
+        guard let managedObjectModel = NSManagedObjectModel(contentsOf: modelURL) else {
+            fatalError("Unable to load data model")
+        }
+        return managedObjectModel
+    }()
+    
+    private lazy var persistentStoreCoordinator: NSPersistentStoreCoordinator = {
+        let persistentStoreCoordinator = NSPersistentStoreCoordinator(managedObjectModel: self.managedObjectModel)
+        
+        let fileManager = FileManager.default
+        let storeName = "\(self.modelName).sqlite"
+        
+        let documentsDirectoryURL = fileManager.urls(for: .documentDirectory, in: .userDomainMask)[0]
+        
+        let persistentStoreURL = documentsDirectoryURL.appendingPathComponent(storeName)
+        
+        do {
+            let options = [NSMigratePersistentStoresAutomaticallyOption: true, NSInferMappingModelAutomaticallyOption: true]
+            try persistentStoreCoordinator.addPersistentStore(ofType: NSSQLiteStoreType, configurationName: nil, at: persistentStoreURL, options: options)
+        } catch {
+            fatalError("Unable to add persistent store")
+        }
+        return persistentStoreCoordinator
+    }()
+    
+}
--- Notes/View Controllers/Root View Controller/ViewController.swift
+++ Notes/View Controllers/Root View Controller/ViewController.swift
@@ -9,17 +9,14 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var coreDataManager = CoreDataManager(modelName: "Notes")
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
     }
-
 
 }
 
