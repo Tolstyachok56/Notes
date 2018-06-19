b9ec12d85576a9771d3ad119fae5eb01eca9d733
Viktoriia Badisova <vbadisova@gmail.com>
2018-06-19 12:57:23 +0300

    Fetch notes

--- Notes/View Controllers/Notes/Notes View Controller/NotesViewController.swift
+++ Notes/View Controllers/Notes/Notes View Controller/NotesViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 import CoreData
 
-class NotesViewController: UIViewController {
+class NotesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
     //MARK: - Segues
     
@@ -19,6 +19,7 @@ class NotesViewController: UIViewController {
     
     //MARK: - Properties
     
+    @IBOutlet var notesView: UIView!
     @IBOutlet var messageLabel: UILabel!
     @IBOutlet var tableView: UITableView!
     
@@ -26,12 +27,37 @@ class NotesViewController: UIViewController {
     
     var coreDataManager = CoreDataManager(modelName: "Notes")
     
+    //MARK: -
+    
+    private var notes: [Note]? {
+        didSet {
+            updateView()
+        }
+    }
+    
+    //MARK: -
+    
+    private var hasNotes: Bool {
+        guard let notes = notes else { return false }
+        return notes.count > 0
+    }
+    
+    //MARK: -
+    
+    private lazy var updatedAtDateFormatter: DateFormatter = {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "MMM d, HH:mm"
+        return dateFormatter
+    }()
+    
     //MARK: - View life cycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        messageLabel.text = "You don't have any notes yet"
+        title = "Notes"
+        setupView()
+        fetchNotes()
     }
     
     //MARK: - Navigation
@@ -47,6 +73,72 @@ class NotesViewController: UIViewController {
             fatalError("Unexpected segue identifier")
         }
     }
+    
+    //MARK: - View methods
+    
+    private func setupView() {
+        setupMessageLabel()
+        setupTableView()
+    }
+    
+    private func updateView() {
+        tableView.isHidden = !hasNotes
+        messageLabel.isHidden = hasNotes
+    }
+    
+    private func setupMessageLabel() {
+        messageLabel.text = "You don't have any notes yet"
+    }
+    
+    private func setupTableView() {
+        
+    }
+    
+    //MARK: - Fetching
+    
+    private func fetchNotes() {
+        let fetchRequest: NSFetchRequest<Note> = Note.fetchRequest()
+        fetchRequest.sortDescriptors = [NSSortDescriptor(key: #keyPath(Note.updatedAt), ascending: false)]
+        
+        coreDataManager.managedObjectContext.performAndWait {
+            do {
+                let notes = try fetchRequest.execute()
+                self.notes = notes
+                self.tableView.reloadData()
+            } catch let fetchError {
+                print("Unable to execute fetch request")
+                print("\(fetchError): \(fetchError.localizedDescription)")
+            }
+        }
+    }
+    
+    //MARK: - UITableViewDataSource methods
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return hasNotes ? 1 : 0
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        guard let notes = notes else { return 0 }
+        return notes.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let note = notes?[indexPath.row] else {
+            fatalError("Unexpected index path")
+        }
+        
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: NoteTableViewCell.reuseIdentifier, for: indexPath) as? NoteTableViewCell else {
+            fatalError("Unexpected index path")
+        }
+        
+        cell.titleLabel.text = note.title
+        cell.contentsLabel.text = note.contents
+        cell.updatedAtLabel.text = updatedAtDateFormatter.string(from: note.updatedAt!)
+        
+        return cell
+    }
+
 
 }
 
